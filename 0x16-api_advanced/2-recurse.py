e75fb743c9f623f2e55edba1ce4a0d4fd4ec986b
Asser Elshafey <elshafeyasser@yahoo.com>
2023-11-07 22:41:31 +0200

    HOT FIX"

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,13 +4,11 @@ import requests
 
 
 def recurse(subreddit, hot_list=[], after=None):
-    # Base case: invalid subreddit
     response = requests.get(f"https://www.reddit.com/r/{subreddit}/hot.json", headers={
                             "User-agent": "custom"}, params={"after": after})
     if response.status_code != 200:
         return None
 
-    # Recursive case: valid subreddit
     data = response.json().get("data")
     children = data.get("children")
     after = data.get("after")
